ea93b02b18c52b6f4747e5c530ffa30b64149291
gtzambranop <gtzambranop@unal.edu.co>
2021-07-14 21:44:32 -0500

    Second option menu - clustering dataset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 import cv2
@@ -6,6 +7,7 @@ from consolemenu import *
 from consolemenu.format import *
 from consolemenu.items import *
 from k_means import KMeans
+from sklearn.preprocessing import StandardScaler
 
 # Reduce la dimensión de la imagen de 3 dimensiones a 2
 def process_image(route_image):
@@ -120,6 +122,198 @@ def image_menu():
     menu.append_item(var_part_item)
     return menu
 
+def k_means_dataset(df, k):
+    model = KMeans(max_iter = 500, tolerance = 0.001, k = k, init_method = "Min Max")
+    (clusters, data) = model.fit(df)
+
+    plt.close()
+    plt.figure(figsize=(12,12))
+    plt.subplot(321)
+    for i, cluster_mean in enumerate(clusters):
+        data_cluster_i = data[ data[:, -1] == i ]
+        plt.scatter(data_cluster_i[:, 2], data_cluster_i[:, 0], label = 'Cluster ' + str(i))
+        plt.plot(cluster_mean[0], cluster_mean[1], label = 'Centroid ' + str(i), marker='*', markersize=15, markeredgecolor="k", markeredgewidth=1)
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=16)
+    plt.title('Partidos ganados', fontsize=16)
+
+    plt.subplot(322)
+    for i, cluster_mean in enumerate(clusters):
+        data_cluster_i = data[ data[:, -1] == i ]
+        plt.scatter(data_cluster_i[:, 2], data_cluster_i[:, 1], label = 'Cluster ' + str(i))
+        plt.plot(cluster_mean[0], cluster_mean[1], label = 'Centroid ' + str(i), marker='*', markersize=15, markeredgecolor="k", markeredgewidth=1)
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=16)
+    plt.title('Eficiencia defensiva', fontsize=16)
+
+    plt.subplot(323)
+    for i, cluster_mean in enumerate(clusters):
+        data_cluster_i = data[ data[:, -1] == i ]
+        plt.scatter(data_cluster_i[:, 2], data_cluster_i[:, 3], label = 'Cluster ' + str(i))
+        plt.plot(cluster_mean[0], cluster_mean[1], label = 'Centroid ' + str(i), marker='*', markersize=15, markeredgecolor="k", markeredgewidth=1)
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=16)
+    plt.title('Lanzamientos efectivos', fontsize=16)
+
+    plt.subplot(324)
+    for i, cluster_mean in enumerate(clusters):
+        data_cluster_i = data[ data[:, -1] == i ]
+        plt.scatter(data_cluster_i[:, 2], data_cluster_i[:, 4], label = 'Cluster ' + str(i))
+        plt.plot(cluster_mean[0], cluster_mean[1], label = 'Centroid ' + str(i), marker='*', markersize=15, markeredgecolor="k", markeredgewidth=1)
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=16)
+    plt.title('Porcentaje de tiros de 2 puntos hechos', fontsize=16)
+
+
+    plt.subplot(325)
+    for i, cluster_mean in enumerate(clusters):
+        data_cluster_i = data[ data[:, -1] == i ]
+        plt.scatter(data_cluster_i[:, 2], data_cluster_i[:, 5], label = 'Cluster ' + str(i))
+        plt.plot(cluster_mean[0], cluster_mean[1], label = 'Centroid ' + str(i), marker='*', markersize=15, markeredgecolor="k", markeredgewidth=1)
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=16)
+    plt.title('Triunfos por encima de la burbuja', fontsize=16)
+
+    plt.show()
+
+def process_data(df):
+    temp = df[['W','ADJOE','BARTHAG','EFG_O','2P_O','WAB']]
+    scaler = StandardScaler()
+    df_scale = scaler.fit_transform(temp)
+    return df_scale
+
+
+def print_selected_variables(datos):
+    plt.figure(figsize=(12,10))
+
+    plt.subplot(321)
+    plt.scatter(y=datos['BARTHAG'], x=datos['W'],alpha=0.5, edgecolor='k')
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=12)
+    plt.title('Eficiencia Ofensiva', fontsize=12)
+
+    plt.subplot(322)
+    plt.scatter(y=datos['BARTHAG'], x=datos['ADJOE'],alpha=0.5, edgecolor='k')
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=12)
+    plt.title('% Tiros efectivos', fontsize=12)
+
+
+    plt.subplot(323)
+    plt.scatter(y=datos['BARTHAG'], x=datos['EFG_O'],alpha=0.5, edgecolor='k')
+    plt.yticks(fontsize=12)
+    plt.ylabel('Prob Ganar', fontsize=12)
+    plt.title('% rebote ofensivo', fontsize=12)
+
+
+    # Partidos ganados
+    plt.subplot(324)
+    plt.scatter(y=datos['BARTHAG'], x=datos['2P_O'],alpha=0.5, edgecolor='k')
+    plt.ylabel('Prob Ganar', fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.title('% rotación', fontsize=12)
+
+
+    plt.subplot(325)
+    plt.scatter(y=datos['BARTHAG'], x=datos['WAB'],alpha=0.5, edgecolor='k')
+    plt.ylabel('Prob ganar', fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.title('% tiros de 2 puntos hechos', fontsize=12)
+
+    plt.show()
+
+
+def print_cmap(df):
+    alpha = ['G', 'W', 'ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D',
+       'TOR', 'TORD', 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O',
+       '3P_D', 'ADJ_T', 'WAB']
+    corr = df.corr()
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(corr, interpolation='nearest')
+    fig.colorbar(cax)
+
+    ax.set_xticklabels(['']+alpha)
+    ax.set_yticklabels(['']+alpha)
+    for (i, j), z in np.ndenumerate(corr):
+        ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    plt.show()
+
+def get_dataframe():
+    df = pd.read_csv('data/basketball_19.csv')
+    df = df.drop(['TEAM','CONF','POSTSEASON','SEED'],axis=1)
+    return df
+
+def dataset_submenu_3(df):
+    mn = {
+        "title": "Clusterización del dataset",
+        "prologue": "Para realizar la clusterización de los datos se eliminaron las columnas que no fueron seleccionadas en el paso anterior y se estandarizaron las variables. Seleccione la cantidad de grupos que desea formar.",
+        "item_1": "2",
+        "item_2": "3",
+        "item_3": "4"
+    }
+    new_df = process_data(df)
+
+    menu = ConsoleMenu(title=mn["title"], prologue_text=mn["prologue"],
+                       formatter=get_menu_format())
+
+    k_2_item = FunctionItem(mn["item_1"], k_means_dataset, [new_df, int(mn["item_1"])])
+    k_3_item = FunctionItem(mn["item_2"], k_means_dataset, [new_df, int(mn["item_2"])])
+    k_4_item = FunctionItem(mn["item_3"], k_means_dataset, [new_df, int(mn["item_3"])])
+    # Seleccionar la cantidad e clusters = k
+    menu.append_item(k_2_item)
+    menu.append_item(k_3_item)
+    menu.append_item(k_4_item)
+    return menu
+
+def dataset_submenu_2(df):
+    mn = {
+        "title": "Variables seleccionadas",
+        "prologue": "Se escogieron 5 variables que están fuertemente relacionadas con el BARTHAG (probabilidad de vencer a un equipo), y son W (Partidos ganados), ADJOE (Eficiencia Defensiva), EFG_O (Lanzamientos efectivos), 2P_O (Porcentaje de tiros de 2 puntos hechos) y WAB (Triunfos por encima de la burbuja).",
+        "item_1": "Ver graficas de las variables selecionadas",
+        "item_2": "Continuar"
+    }
+
+    menu = ConsoleMenu(title=mn["title"], prologue_text=mn["prologue"],
+                       formatter=get_menu_format())
+    item_1 = FunctionItem(mn["item_1"], print_selected_variables, [df])
+    item_2 = SubmenuItem(mn["item_2"], dataset_submenu_3(df), menu)
+
+    menu.append_item(item_1)
+    menu.append_item(item_2)
+    return menu
+
+def dataset_submenu_1(df):
+    mn = {
+        "title": "Exploración de datos",
+        "prologue": "Para empezar el análisis hay que hacer una exploración inicial de los datos. Nuestra variable objetivo es el BARTHAG (probabilidad de vencer a un equipo), por lo que elegiremos variables que estén fuertemente relacionadas con este, para ello utilizaremos un mapa de calor que podrá ver seleccionando la primera opción del menú.",
+        "item_1": "Ver mapa de calor",
+        "item_2": "Continuar"
+    }
+    menu = ConsoleMenu(title=mn["title"], prologue_text=mn["prologue"],
+                       formatter=get_menu_format())
+    item_1 = FunctionItem(mn["item_1"], print_cmap, [df])
+    item_2 = SubmenuItem(mn["item_2"], dataset_submenu_2(df), menu)
+
+    menu.append_item(item_1)
+    menu.append_item(item_2)
+    return menu
+
+def dataset_menu():
+    mn = {
+        "title": "Clusterización sobre un dataset de baloncesto",
+        "prologue": " Se tienen los datos de desempeño de los equipos de baloncesto del torneo NCAA March Madness que contiene las estadísticas de juego de 353 equipos de la liga. El objetivo es encontrar patrones en el desempeño de los equipos y generar recomendaciones de umbrales en las estadísticas para que un equipo esté en el grupo de desempeño superior.",
+        "item_1": "Exploración de datos"
+    }
+
+    df = get_dataframe()
+
+    menu = ConsoleMenu(title=mn["title"], prologue_text=mn["prologue"],
+                       formatter=get_menu_format())
+    item = SubmenuItem(mn["item_1"], dataset_submenu_1(df), menu)
+
+    menu.append_item(item)
+    return menu
 
 def principal_menu():
     mn = {
@@ -133,7 +327,10 @@ def principal_menu():
     menu = ConsoleMenu(title=mn["title"], prologue_text=mn["prologue"],
                        formatter=get_menu_format())
     img_item = SubmenuItem(mn["item_1"], image_menu(), menu)
+    df_item = SubmenuItem(mn["item_2"], dataset_menu(), menu)
+
     menu.append_item(img_item)
+    menu.append_item(df_item)
     menu.show()
 
 
